1bece9ee3d2f10b0bed91435ce718a2ec4eb29f7
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2023-05-03 10:41:19 +0300

    Create Solution.cs

---
+++ Problems LeetCode/ZigZag Conversion/Solution.cs
@@ -0,0 +1,41 @@
+using System.Text;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Convert("Cezarescuestea", 4));
+    }
+
+    public static string Convert(string s, int numRows)
+    {
+        if(numRows == 1)
+        {
+            return s;                   
+        }
+        var result = new StringBuilder();
+        var perIter = numRows * 2 - 2;
+
+        for(int row = 0; row < numRows; row++) 
+        {
+            int i = 0;
+            while((i+row) < s.Length)
+            {
+                result.Append(s[i+row]);
+                if (row != 0 && row != numRows - 1)
+                {
+                    if((i+perIter-row)< s.Length)
+                    {
+                        result.Append(s[i+perIter-row]);    
+                    }
+                }
+                i += perIter;
+            }
+            
+        }
+        return result.ToString();
+
+
+        
+    }
+}
