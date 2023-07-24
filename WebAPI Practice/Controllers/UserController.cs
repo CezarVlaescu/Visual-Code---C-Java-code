7dc46e34417cec0d3f53763b8ade190944dc6d7a
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2023-07-24 11:16:31 +0300

    Add files via upload

---
+++ WebAPI Practice/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIDApp.Data;
+using WebAPIDApp.Entities;
+
+namespace WebAPIDApp.Controllers
+{
+    public class UserController : BaseAPIController
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<AppUser>> GetOneUser(int id)
+        {
+            return await _context.Users.FindAsync(id);                      
+        }
+    }
+}
---
+++ WebAPI Practice/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using WebAPIDApp.Entities;
+
+namespace WebAPIDApp.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
