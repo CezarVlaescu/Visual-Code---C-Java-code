ce41343eb6c9a57b97b74b18a4cc654eec1dfcdb
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2024-03-13 20:28:38 +0200

    Update Odd or even.cs

--- Odd or even/Odd or even.cs
+++ Odd or even/Odd or even.cs
@@ -29,5 +29,10 @@ class Solution
 
         Console.ReadLine();
 
+        double pivot = Math.Sqrt((n * (n + 1)) / 2);
+        if(pivot - Math.Floor(pivot) == 0)
+            return (int)pivot;
+        return -1;
+
     }
 }
